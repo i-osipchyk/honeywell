44bf0431ef0f0bccfcabb0dc1864977a6de4e5f1
Ivan Osipchyk <ivanosipchyk@MacBook-Air.local>
2023-07-29 18:29:44 +0200

    added function to check file format

--- backend/server.py
+++ backend/server.py
@@ -8,5 +8,9 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
